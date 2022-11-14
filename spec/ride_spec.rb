8adb5bec4ffbbdada70666b5c368da5896b6ca7b
Christian McCabe <christian.mccabe89@gmail.com>
2022-11-14 10:54:00 -0700

    Create more test conditions for #board_rider

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -39,5 +39,21 @@ RSpec.describe do
     expect(@visitor1.spending_money).to eq(8)
     expect(@visitor2.spending_money).to eq(4)
     expect(@ride1.total_revenue).to eq(3)
+
+    @visitor2.add_preference(:thrilling)
+    @visitor3.add_preference(:thrilling)
+
+    expect(@visitor2.preferences).to eq([:gentle, :thrilling])
+    expect(@visitor3.preferences).to eq([:thrilling])
+
+    @ride3.board_rider(@visitor1)
+    @ride3.board_rider(@visitor2)
+    @ride3.board_rider(@visitor3)
+
+    expect(@visitor1.spending_money).to eq(8)
+    expect(@visitor2.spending_money).to eq(4)
+    expect(@visitor3.spending_money).to eq(13)
+    expect(@ride3.rider_log).to eq({@visitor3 => 1})
+    expect(@ride3.total_revenue).to eq(2)
   end
 end
