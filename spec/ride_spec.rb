9fc1bf2a3c516d8467a24954225c81640a3bd839
Christian McCabe <christian.mccabe89@gmail.com>
2022-11-14 09:48:34 -0700

    Create test for default rider log

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -5,6 +5,10 @@ require 'rspec'
 RSpec.describe do
   before :each do
     @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor1.add_preference(:gentle)
+    @visitor2.add_preference(:gentle)
   end
 
   it 'exists and has a default name, min height requirement, admission fee, excitement attibute' do
@@ -17,5 +21,9 @@ RSpec.describe do
 
   it 'has a default total revenue counter that starts at 0' do
     expect(@ride1.total_revenue).to eq(0)
+  end
+
+  it 'has a default rider log that is an empty hash' do
+    expect(@ride1.rider_log).to eq({})
   end
 end
