5f477a2890fa760c0446d51f31c6aac53307a015
Christian McCabe <christian.mccabe89@gmail.com>
2022-11-14 09:22:15 -0700

    Initialize Visitor class; Pass respective test

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,3 +1,9 @@
 class Visitor
+  attr_reader :name, :age, :spending_money
 
+  def initialize(name, age, spending_money)
+    @name = name
+    @age = age
+    @spending_money = spending_money
+  end
 end
